38ee800d536c0ed3b1990b61b96464ff45440974
33781 <33781@SURFACE-PRO-4-FAMCD.education.lakanal.epf.fr>
2022-10-28 17:05:53 +0200

    Réenregistrement du 1 : Création d’une première classe et de quelques objets associés

---
+++ TP2_Bieres_COLAS-DEMANGEOT/src/tp2_bieres_colas/demangeot/BouteilleBiere.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_colas.demangeot;
+
+/**
+ *
+ * @author 33781
+ */
+public class BouteilleBiere {
+    
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
+public void lireEtiquette() {
+
+    System.out.println("Bouteille de " + nom +" (" + degreAlcool +" degres) \nBrasserie : "+ brasserie ) ;
+}
+
+public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+ nom = unNom;
+ degreAlcool = unDegre;
+ brasserie = uneBrasserie;
+ ouverte = false;
+}
+public boolean Décapsuler() {
+    if (ouverte == false){
+    ouverte = true;
+    return ouverte; 
+    }else if (ouverte == true) {
+            System.out.println("Bière déjà ouverte");
+            ouverte = false;
+            }
+        return ouverte;
+}
+@Override
+public String toString() {
+String chaine_a_retourner;
+chaine_a_retourner = nom + " (" + degreAlcool + " degrés) Ouverte ? ";
+if (ouverte == true ) chaine_a_retourner += "oui" ;
+else chaine_a_retourner += "non" ;
+return chaine_a_retourner ;
+}
+}
