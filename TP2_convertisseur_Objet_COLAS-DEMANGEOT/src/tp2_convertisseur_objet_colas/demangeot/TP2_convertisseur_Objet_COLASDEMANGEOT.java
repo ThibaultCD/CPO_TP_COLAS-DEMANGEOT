ab04c960fcf0f56fb13dfe1d1d497b23a679d60d
33781 <33781@SURFACE-PRO-4-FAMCD.education.lakanal.epf.fr>
2022-10-28 15:35:43 +0200

    Réalisation du 2 : Un convertisseur de températures : version classes

---
+++ TP2_convertisseur_Objet_COLAS-DEMANGEOT/src/tp2_convertisseur_objet_colas/demangeot/TP2_convertisseur_Objet_COLASDEMANGEOT.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_convertisseur_objet_colas.demangeot;
+
+/**
+ *
+ * @author 33781
+ */
+public class TP2_convertisseur_Objet_COLASDEMANGEOT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        double resultat = 0;
+        
+        Convertisseur Convert1 = new Convertisseur();
+        while(resultat != resultat-300){
+            
+            resultat = Convert1.convert();
+            System.out.println(Convert1.toString());
+            System.out.println("Le résultat de la conversion est " + resultat + "\n");
+            
+        }
+    }
+    
+}
