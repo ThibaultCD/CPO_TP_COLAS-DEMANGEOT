3aacf18d7ff56a79c9917df632c0f18952edd866
33781 <33781@SURFACE-PRO-4-FAMCD.education.lakanal.epf.fr>
2022-10-30 18:45:45 +0100

    Ajout de commentaires à : 1 Création d’une première classe et de quelques objets associés

--- TP2_Bieres_COLAS-DEMANGEOT/src/tp2_bieres_colas/demangeot/BouteilleBiere.java
+++ TP2_Bieres_COLAS-DEMANGEOT/src/tp2_bieres_colas/demangeot/BouteilleBiere.java
@@ -8,24 +8,23 @@ package tp2_bieres_colas.demangeot;
  *
  * @author 33781
  */
-public class BouteilleBiere {
-    
+public class BouteilleBiere { //Création d'une classe
+//Création des attributs    
     String nom;
     double degreAlcool;
     String brasserie;
     boolean ouverte;
-public void lireEtiquette() {
-
+public void lireEtiquette() {//Création d'une méthode qui affichage les caractéristiques d'une bouteille
     System.out.println("Bouteille de " + nom +" (" + degreAlcool +" degres) \nBrasserie : "+ brasserie ) ;
 }
 
-public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {//Création du constructeur
  nom = unNom;
  degreAlcool = unDegre;
  brasserie = uneBrasserie;
  ouverte = false;
 }
-public boolean Décapsuler() {
+public boolean Décapsuler() {//Nouvelle méthode qui regarde si la bouteille est déjà ouverte ou non et retourne le résultat
     if (ouverte == false){
     ouverte = true;
     return ouverte; 
@@ -36,8 +35,8 @@ public boolean Décapsuler() {
         return ouverte;
 }
 @Override
-public String toString() {
-String chaine_a_retourner;
+public String toString() {//Lorsqu'une bouteille est appelée dans un print, le programme retourne maintenant les informations contenues dans cette méthode toString
+    String chaine_a_retourner;
 chaine_a_retourner = nom + " (" + degreAlcool + " degrés) Ouverte ? ";
 if (ouverte == true ) chaine_a_retourner += "oui" ;
 else chaine_a_retourner += "non" ;
--- TP2_Bieres_COLAS-DEMANGEOT/src/tp2_bieres_colas/demangeot/TP2_Bieres_COLASDEMANGEOT.java
+++ TP2_Bieres_COLAS-DEMANGEOT/src/tp2_bieres_colas/demangeot/TP2_Bieres_COLASDEMANGEOT.java
@@ -15,20 +15,21 @@ public class TP2_Bieres_COLASDEMANGEOT {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Création de l'objet BouteilleBiere et de trois références objets
         BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls" ,7.0 , "Dubuisson");
         uneBiere.ouverte = false ;
-        uneBiere.lireEtiquette();
+        uneBiere.lireEtiquette();//Appelle de la méthode lirEtiquette
         
         BouteilleBiere autreBiere = new BouteilleBiere("Leffe",6.6,"Abbaye de Leffe");
         autreBiere.ouverte = false ;
         autreBiere.lireEtiquette();
-        System.out.println(autreBiere) ;
+        System.out.println(autreBiere) ;//Affichage des caractéristiques à travers la méthode toString
         
         BouteilleBiere TroisièmeBiere = new BouteilleBiere ("Corona", 5.8 , "Modelo" );
         TroisièmeBiere.lireEtiquette();
         TroisièmeBiere.ouverte = true ;
         System.out.println(TroisièmeBiere);
-        TroisièmeBiere.Décapsuler();
+        TroisièmeBiere.Décapsuler();//Appelle de la méthode Décapsuler
         
     }
     
