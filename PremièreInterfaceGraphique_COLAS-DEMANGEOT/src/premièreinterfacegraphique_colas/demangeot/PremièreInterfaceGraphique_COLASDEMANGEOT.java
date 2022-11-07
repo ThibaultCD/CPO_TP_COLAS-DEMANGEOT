fc7790e4318ef53a227fd22d7b39ad3b1116c610
33781 <33781@SURFACE-PRO-4-FAMCD.education.lakanal.epf.fr>
2022-11-07 12:32:34 +0100

    Fin apprentissage interface

--- PremièreInterfaceGraphique_COLAS-DEMANGEOT/src/premièreinterfacegraphique_colas/demangeot/PremièreInterfaceGraphique_COLASDEMANGEOT.java
+++ PremièreInterfaceGraphique_COLAS-DEMANGEOT/src/premièreinterfacegraphique_colas/demangeot/PremièreInterfaceGraphique_COLASDEMANGEOT.java
@@ -13,8 +13,5 @@ public class PremièreInterfaceGraphique_COLASDEMANGEOT {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+
 }
