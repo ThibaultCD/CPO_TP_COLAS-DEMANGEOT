a9a065c27dcf56cf242f292298e2a5a83e15d2df
33781 <33781@SURFACE-PRO-4-FAMCD.education.lakanal.epf.fr>
2022-11-07 13:01:50 +0100

    Finalisation du projet interface

--- ProjetInterface_COLAS-DEMANGEOT/src/projetinterface_colas/demangeot/ProjetInterface_COLASDEMANGEOT.java
+++ ProjetInterface_COLAS-DEMANGEOT/src/projetinterface_colas/demangeot/ProjetInterface_COLASDEMANGEOT.java
@@ -13,8 +13,4 @@ public class ProjetInterface_COLASDEMANGEOT {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
 }
