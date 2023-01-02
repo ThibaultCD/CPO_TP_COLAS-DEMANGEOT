b5560770e2b1ca9f85b78ab8efc08f69c9da371d
33781 <33781@SURFACE-PRO-4-FAMCD.education.lakanal.epf.fr>
2023-01-02 16:52:25 +0100

    Mode Grpahique du mini projet

---
+++ LightsOff_modeGraphique/src/lightsoff/CelluleGraphique.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lightsoff;
+
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author 33781
+ */
+public class CelluleGraphique extends JButton {
+    CelluleLumineuse celluleAssociee;
+    ImageIcon img_allumee = new javax.swing.ImageIcon(getClass().getResource("/images/image ampoule allumée100x100.jpg"));
+    ImageIcon img_eteint = new javax.swing.ImageIcon(getClass().getResource("/images/photo eteinte100x100.jpg"));
+    
+    @Override
+    public void paintComponent(Graphics G){
+        super.paintComponent(G);
+        setIcon(img_eteint);
+    }
+}
---
+++ LightsOff_modeGraphique/src/lightsoff/CelluleLumineuse.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lightsoff;
+
+/**
+ *
+ * @author 33781
+ */
+public class CelluleLumineuse {
+    boolean[][]grille;
+    
+    public CelluleLumineuse(int taille){
+        grille = new boolean [taille][taille];
+        
+        for (int i = 0; i <taille ; i++){
+            for (int j = 0; j < taille; j++){
+                grille[i][j]=false;
+            }
+        }
+    }
+    
+    public String toString(){
+        String s ="";
+        for(int i = 0; i < grille.length; i++){
+            for(int j = 0; j < grille[i].length; j++){
+                if(grille[i][j]){
+                    s +="0";
+                }else{
+                    s +="X";
+                }
+            }
+            s +="\n";
+        }
+        return s;
+    }
+    
+    public void LumièreDebutDePartie(){
+         for(int i = 0; i < grille.length; i++){
+            for(int j = 0; j < grille[i].length; j++){
+                if (Math.random() > 0.5){
+                 grille[i][j] = true;   
+                }
+            }
+        }    
+    } 
+    
+    public void choix (int x,int y){
+        grille[x][y]=!grille[x][y];
+        
+        if (x < grille.length - 1 ){
+        grille[x + 1][y]=!grille[x + 1][y];
+        }
+        
+        if (x > 0){
+        grille[x - 1][y]=!grille[x - 1][y];
+        }
+        
+        if (y < grille.length - 1){
+        grille[x][y + 1]=!grille[x][y + 1];
+        }
+        
+        if(y > 0){
+        grille[x][y - 1]=!grille[x][y - 1];
+        }
+        
+    }
+}
